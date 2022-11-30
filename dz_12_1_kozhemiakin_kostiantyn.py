ceb741a1503c7e65452794abd37c69fa308c0310
KonstantinKozhemiakin <kozhemiakin.konstantin@gmail.com>
2022-11-30 15:40:10 +0200

    second commit

--- dz_12_1_kozhemiakin_kostiantyn.py
+++ dz_12_1_kozhemiakin_kostiantyn.py
@@ -2,7 +2,7 @@ import concurrent.futures
 import time
 import os
 
-NUM = 20
+NUM = 200
 COUNT = 5
 
 
@@ -13,9 +13,9 @@ def factorial(num):
         return num * factorial(num - 1)
 
 
-def without_concurrent_futures(*func, count=1):
+def without_concurrent_futures(func, num, count=1):
     for i in range(count):
-        print(*func)
+        print(func(num))
 
 
 def thread_pool_executor(func, num, count=1):
@@ -36,18 +36,35 @@ def process_pool_executor(func, num, count=1):
         print(f.result())
 
 
-def compare_function(*args):
-    min_time = 0
-    min_time_func = args[0]
-    print(args)
-    time_start = time.time()
-    for i in args:
-        print("ff")
-    time_finish = time.time() - time_start
-    print(time_finish)
+def compare_function(*func, funct = "", num = NUM, count = COUNT):
+    best_time = 9999999999
+    best_func = func[0]
+    for fun in func:
+        time_start = time.time()
+        fun(funct, num, count)
+        time_prog = time.time() - time_start
+        if time_prog < best_time:
+            best_time = time_prog
+            best_func = fun
+        print('----------------------')
+        print(time_prog)
+    print(best_func, best_time)
+
+    # if time_process > time_thread:
+    #     print("ThreadPoolExecutor is better")
+    # else:
+    #     print("ProcessPoolExecutor is better")
+    # min_time = 0
+    # min_time_func = args[0]
+    # time_start = time.time()
+    # for i in args:
+    #     print("ff")
+    # time_finish = time.time() - time_start
+    # print(time_finish)
+
 
 
 if __name__ == '__main__':
-    compare_function(without_concurrent_futures(factorial(NUM), count=COUNT),
-                     thread_pool_executor(factorial, NUM, COUNT),
-                     process_pool_executor(factorial, NUM, COUNT))
+    compare_function(without_concurrent_futures,
+                     thread_pool_executor,
+                     process_pool_executor,funct=factorial, num=NUM, count=COUNT)
